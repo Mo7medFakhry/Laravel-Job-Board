cf6833d4876e8bcfa21811ade1067ebf33ef1f8f
Mo7medFakhry <mohamedhawas415@gmail.com>
2025-06-28 20:54:01 +0300

    Blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <h2>Welcome To my page</h2>
+
+    <ul>
+        <li>Mohamed</li>
+        <li>Ali</li>
+    </ul>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
+
+Route::get('/about', [IndexController::class, 'about']);
+
+Route::get('/contact', [IndexController::class, 'contact']);
 
 Route::get('/job', [JobController::class, 'index']);
